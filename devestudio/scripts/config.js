ccfa0821065240470fd0cec173a3c694be439c1d
Bruna Brito <brunasrbrito@gmail.com>
2019-02-25 10:58:03 -0300

    atualização

---
+++ devestudio/scripts/config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Wata = {
+
+    // Configuration of triangles
+    triangles: {
+        mesh: {
+            depth: 11,
+            slices: 250
+        },
+        light: {
+            ambient: '#ff0000', 
+            // EA4D2A
+            diffuse: '#F36E1E',
+            distance: 55
+        }
+    },
+
+    // Various toast messages
+    toastMessages: {
+        fillInRequiredFields: 'Por favor, preencha os campos obrigatórios',
+        enterValidEmail: 'Por favor insira um endereço de e-mail válido',
+        messageSent: "Sua mensagem foi enviada. Nós entraremos em contato com você em breve.",
+        somethingWrong: 'Algo deu errado, tente novamente. Erro:'
+    },
+
+};
